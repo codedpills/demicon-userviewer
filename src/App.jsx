6de8c09e8952c05f1c5f5b51e9d4950968705e16
codedpills <codedpills@gmail.com>
2022-04-12 17:26:42 +0200

    Filter and display users

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,32 @@
 import "./App.css";
 import Users from "./users";
+import { useUsers } from "./users/useUsers";
+import { SearchInput } from "./shared/SearchInput";
 
 function App() {
+  const { loading, users, fetchUsers } = useUsers();
+
   return (
     <div className="App">
+      <h2>D3m1c0n Random User Viewer</h2>
       <table>
         <thead>
-            <tr>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Gender</th>
-                <th>Location</th>
-            </tr>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Gender</th>
+            <th>Location</th>
+            <th>
+              <SearchInput
+                loading={loading}
+                onSearch={fetchUsers}
+                fetchOnEmpty
+              />
+            </th>
+          </tr>
         </thead>
         <tbody>
-          <Users />
+          <Users users={users} loading={loading} />
         </tbody>
       </table>
     </div>
@@ -22,4 +34,3 @@ function App() {
 }
 
 export default App;
-
